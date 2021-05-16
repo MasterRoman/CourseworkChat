d2dde2238d6074fde94cfbe026bc72cdee52e850
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-17 01:33:26 +0300

    add tabBar controller

---
+++ CourseworkChat/CourseworkChat/BaseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  BaseCoordinator.swift
+//  CourseworkChat
+//
+//  Created by Admin on 16.05.2021.
+//
+
+import Foundation
---
+++ CourseworkChat/CourseworkChat/MainScreen/MainTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  MainTabBarController.swift
+//  CourseworkChat
+//
+//  Created by Admin on 17.05.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.barStyle = .black
+        self.tabBar.barTintColor = .black
+        self.tabBar.tintColor = UIColor.white
+        
+    }
+    
+}
