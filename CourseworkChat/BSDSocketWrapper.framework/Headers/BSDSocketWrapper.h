d7a3655f62ab5d8ee87b92c880ff9f7e2e2873b4
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-22 14:06:02 +0300

    files after clean build

---
+++ CourseworkChat/BSDSocketWrapper.framework/Headers/BSDSocketWrapper.h
@@ -0,0 +1,18 @@
+//
+//  BSDSocketWrapper.h
+//  BSDSocketWrapper
+//
+//  Created by Admin on 30.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BSDSocketWrapper.
+FOUNDATION_EXPORT double BSDSocketWrapperVersionNumber;
+
+//! Project version string for BSDSocketWrapper.
+FOUNDATION_EXPORT const unsigned char BSDSocketWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSDSocketWrapper/PublicHeader.h>
+
+
