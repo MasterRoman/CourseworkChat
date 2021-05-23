0175eedc2dfc8815e43dc5adb55bb6c49f8ad545
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-23 16:16:37 +0300

    refactor network manager

--- CourseworkChat/CourseworkChat/Services/NetworkManager.swift
+++ CourseworkChat/CourseworkChat/Services/NetworkManager.swift
@@ -9,61 +9,76 @@ import Foundation
 import BSDSocketWrapper
 
 protocol NetworkClient {
-    func start()throws ->Bool
-    func stop()throws ->Bool
     func send(message : SendReceiveProtocol) throws
-    func receive(complationHandler:(Result<SendReceiveProtocol,Error>) -> (Void)) throws
 }
 
-class NetworkManager : NetworkClient{
-    var address : String
-    var port : String
+class NetworkManager : NetworkClient {
     
     let socket : ClientEndpoint
-    
     let encoder = JSONEncoder()
     let decoder = JSONDecoder()
     
     init(with address : String = "192.168.78.132",port : String = "2784") throws {
-        self.address = address
-        self.port = port
         self.socket = try ClientEndpoint(host: address, port: port, sockType: .stream)
-    }
-    
-    func start() -> Bool{
         do {
             try socket.connect()
         } catch (_) {
-            return false
+            return
         }
-        return true
     }
     
-    func stop() -> Bool{
-        do {
-            try socket.shutdown(with: .shutBoth)
-            try socket.close()
-        } catch (_) {
-            return false
+    func recieveUntillOffline() throws{
+        DispatchQueue.global(qos: .utility).async{ [weak self] in
+            guard let self = self else {return}
+            while true {
+                do{
+                    try self.socket.receive({ data in
+                        var result : SendReceiveProtocol
+                        do {
+                            result = try self.decoder.decode(SendReceiveProtocol.self, from: data)
+                            switch result{
+                            case .checkLogin(login: let login):
+                                NotificationCenter.default.post(name: .checkLogin, object: login)
+                            case .registration(credentials : let credentials):
+                                NotificationCenter.default.post(name: .registration, object: credentials)
+                            case .authorization(credentials : let credentials):
+                                NotificationCenter.default.post(name: .authorization, object: credentials)
+                            case .newChat(chat: let chat):
+                                NotificationCenter.default.post(name: .newChat, object: chat)
+                            case .newMessage(message: let message):
+                                NotificationCenter.default.post(name: .newMessage, object: message)
+                            case .offline(login: let login):
+                                NotificationCenter.default.post(name: .offline, object: login)
+                            }
+                        } catch (_) {
+                            
+                        }
+                    })
+                }
+                catch{
+                    
+                }
+            }
         }
-        return true
     }
+    
     
     func send(message : SendReceiveProtocol) throws{
         let data = try encoder.encode(message)
         try socket.send(data: data)
     }
     
-    func receive(complationHandler:(Result<SendReceiveProtocol,Error>) -> (Void)) throws {
-        try socket.receive({ data in
-            var result : SendReceiveProtocol
-            do {
-                result = try decoder.decode(SendReceiveProtocol.self, from: data)
-                complationHandler(.success(result))
-            } catch (let error) {
-                complationHandler(.failure(error))
-            }
-        })
-        
+    
+    deinit {
+        do{
+            try socket.shutdown(with: .shutBoth)
+            try socket.close()
+        }
+        catch (let error)
+        {
+            print(error)
+        }
     }
+    
+    
 }
