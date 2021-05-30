bf9ad014ef2880e8094b1570501556da281cd5ad
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-30 04:05:08 +0300

    emit back action

--- CourseworkChat/CourseworkChat/MainScreen/ChatsTab/DetailChat/DetailChatCoordinator.swift
+++ CourseworkChat/CourseworkChat/MainScreen/ChatsTab/DetailChat/DetailChatCoordinator.swift
@@ -11,14 +11,16 @@ import RxSwift
 class DetailChatCoordinator : BaseCoordinator<Void> {
     private let navigationController : UINavigationController
     private let networkManager : NetworkClient
-    init(with navigationController : UINavigationController,networkManager : NetworkClient) {
+    init(with chat : Chat,navigationController : UINavigationController,networkManager : NetworkClient) {
         self.navigationController = navigationController
         self.networkManager = networkManager
     }
     
     override func start() -> Observable<Void> {
         let viewController = DetailChatViewController()
+        let viewModel = DetailChatViewModel()
+        viewController.viewModel = viewModel
         self.navigationController.pushViewController(viewController, animated: true)
-        return Observable.never()
+        return viewModel.output.back
     }
 }
