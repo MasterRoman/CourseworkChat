ef3b580aeae9c5e5dbd9b047d71e58b96b4c1371
Roman Kozko <romanchek_bananchek@mail.ru>
2021-05-31 01:49:26 +0300

    add more configurations

--- CourseworkChat/CourseworkChat/MainScreen/ChatsTab/Cell/ViewModel/ChatCellViewModel.swift
+++ CourseworkChat/CourseworkChat/MainScreen/ChatsTab/Cell/ViewModel/ChatCellViewModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct ChatCellViewModel {
     let chatId : UUID
-    let icon : UIImage?
+    var icon : UIImage?
     var dialogTitle : String
     var lastMessagePreview: String
     var lastMessageTime: String
@@ -26,11 +26,24 @@ struct ChatCellViewModel {
     init(from chat : Chat) {
         self.chatId = chat.chatBody.chatId
         self.icon = nil
-        self.dialogTitle = chat.senders[1].displayName
+        self.dialogTitle = chat.senders.last!.displayName
         self.lastMessagePreview = chat.chatBody.messages.last?.kind.getValue() ?? ""
        
         let formatter = DateFormatter()
         formatter.dateStyle = .short
         self.lastMessageTime = formatter.string(from:chat.chatBody.messages.last!.sentDate)
+    }
+    
+    mutating func configure(with body: ChatBody){
+        let message = body.messages.last
+        self.lastMessagePreview = (message?.kind.getValue())!
+        self.lastMessageTime = getString(from: message!.sentDate)
+        
+    }
+    
+    private func getString(from date : Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter.string(from:date)
     }
 }
