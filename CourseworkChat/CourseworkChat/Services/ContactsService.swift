4d5c48bcadef64c9e27944f79062564dba1e8e53
Roman Kozko <romanchek_bananchek@mail.ru>
2021-06-01 03:04:51 +0300

    add contact service for sending and receiving

---
+++ CourseworkChat/CourseworkChat/Services/ContactsService.swift
@@ -0,0 +1,61 @@
+//
+//  ContactsService.swift
+//  CourseworkChat
+//
+//  Created by Admin on 01.06.2021.
+//
+
+import Foundation
+
+class ContactsService{
+    private let networkManager : NetworkClient
+    private var contactsSource = Dictionary<String,Contact>()
+    
+    var getNewContact : ((_ contact : Contact) -> (Void))?
+    
+    init(with networkManager : NetworkClient) {
+        self.networkManager = networkManager
+    }
+    
+    func getContacts() -> [Contact] {
+        return Array(contactsSource.values)
+    }
+    
+    func getContact(by login:String) -> Contact?{
+        return contactsSource[login]
+    }
+    
+    func addNewContact(login : String){
+        do {
+            try networkManager.send(message: .newContact(login: login))
+        } catch (let error) {
+            print(error)
+        }
+    }
+    
+    private func registerNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(handleNewContactNotification), name: .newContact, object: nil)
+    }
+    
+    private func removeNotifications(){
+        NotificationCenter.default.removeObserver(self, name: .newContact, object: nil)
+    }
+    
+    @objc private func handleNewContactNotification(notification: NSNotification){
+        DispatchQueue.global(qos: .utility).async { [self] in
+            if let contact = notification.object as? Contact {
+                DispatchQueue.global().async(flags: .barrier, execute: {
+                    let login = contact.login
+                    contactsSource[login] = contact
+                })
+                self.getNewContact?(contact)
+            }
+        }
+        
+        
+    }
+
+    deinit {
+        removeNotifications()
+    }
+}
