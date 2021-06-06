38e475d10bf8b4e23625504dad16dad4de519ae5
Roman Kozko <romanchek_bananchek@mail.ru>
2021-06-06 23:52:50 +0300

    add service for working with core data

--- CourseworkChat/CourseworkChat/Services/StorageService.swift
+++ CourseworkChat/CourseworkChat/Services/StorageService.swift
@@ -12,6 +12,7 @@ class StorageService{
     
     private let decoder = JSONDecoder()
     private let encoder = JSONEncoder()
+    private let backQueue = DispatchQueue.init(label: "store", qos: .background, attributes: .concurrent)
     
     // MARK: - Core Data stack
     private lazy var persistentContainer: NSPersistentContainer = {
@@ -28,60 +29,291 @@ class StorageService{
         return persistentContainer.newBackgroundContext()
     }()
     
-    func addChat(chat : Chat){
-        let context = backgroundContext
+    var login : String?
+    
+    func addUser(){
         
+        // queue.async {  [weak self] in
+        //   guard let self = self else {return}
+        
+        let context =  self.backgroundContext
+        
+        let fetchRequest = NSFetchRequest<User>(entityName: "User")
         
         do{
-            let _chat = Chats(context: context)
-            _chat.id = chat.chatBody.chatId
-            _chat.body = try encoder.encode(chat.chatBody)
-            _chat.senders = try encoder.encode(chat.senders)
+            var users = try context.fetch(fetchRequest)
+            guard (users.firstIndex(where: {$0.loginUser == self.login}) != nil) else
+            {
+                let user = User(context: context)
+                user.loginUser = self.login!
+                
+                users.append(user)
+                
+                print("USER")
+                
+                do{
+                    try context.save()
+                }
+                catch (let error)
+                {
+                    print(error)
+                    
+                }
+                
+                
+                return
+            }
             
-            try context.save()
+            
             
         }catch (let error){
             print(error)
+            
             return
         }
-      
+        
+        //   }
+    }
+    
+    func addChat(chat : Chat){
+        self.backQueue.async(flags: .barrier, execute: { [weak self] in
+            
+            guard let self = self else {return}
+            let context = self.backgroundContext
+            
+            
+            let fetchRequest = NSFetchRequest<User>(entityName: "User")
+            fetchRequest.predicate =  NSPredicate(format: "loginUser = %@", self.login!)
+            
+            do{
+                let _chat = Chats(context: context)
+                let id  = chat.chatBody.chatId
+                _chat.id = id
+                let body = ChatBody(chatId: id, messages: [Message]())
+                _chat.body = try self.encoder.encode(body)
+                _chat.senders = try self.encoder.encode(chat.senders)
+                
+                let users = try context.fetch(fetchRequest)
+                for user in users {
+                    if user.loginUser == self.login{
+                        _chat.addToUser(user)
+                    }
+                }
+                
+                
+                do{
+                    
+                    try context.save()
+                }
+                catch (let error)
+                {
+                    print(error)
+                    
+                }
+                
+                
+            }catch (let error){
+                print(error)
+                
+                return
+            }
+            
+        })
         
     }
     
     func getChats() -> [UUID:Chat]?{
-        let context = backgroundContext
+        
+        let context =  self.backgroundContext
         
         
-        let fetchRequest = NSFetchRequest<Chats>(entityName: "Chats")
         
-        var chats = [Chats]()
+        let fetchRequest = NSFetchRequest<User>(entityName: "User")
+        fetchRequest.predicate =  NSPredicate(format: "loginUser = %@", login!)
+        
+        var realChats = [UUID:Chat]()
+        var users = [User]()
         do {
-            chats = try context.fetch(fetchRequest)
+            users = try context.fetch(fetchRequest)
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
             return nil
         }
         
-        var realChats = [UUID:Chat]()
-        
-        for chat in chats {
-            do{
-                let body = try decoder.decode(ChatBody.self, from: chat.body!)
-                let senders = try decoder.decode([Sender].self, from: chat.senders!)
-                let realChat = Chat(senders: senders, chatBody: body)
-                realChats[body.chatId] = realChat
-            }
-            catch (let error)
-            {
-                print(error)
-                return nil
+        for user in users {
+            if user.loginUser == self.login{
+                let _chats = user.chats?.allObjects as? [Chats]
+                guard let chats = _chats else {
+                    return nil
+                }
                 
+                
+                for chat in chats {
+                    do{
+                        let body = try self.decoder.decode(ChatBody.self, from: chat.body!)
+                        let senders = try self.decoder.decode([Sender].self, from: chat.senders!)
+                        let realChat = Chat(senders: senders, chatBody: body)
+                        realChats[body.chatId] = realChat
+                    }
+                    catch (let error)
+                    {
+                        print(error)
+                        
+                    }
+                    
+                }
+                
+                
+                return realChats
             }
+            
+            
+            
         }
         
-        return realChats
+        return nil
     }
     
+    
+    func getContacts() -> [String:Contact]?{
+        let context =  self.backgroundContext
+        
+        let fetchRequest = NSFetchRequest<User>(entityName: "User")
+        fetchRequest.predicate =  NSPredicate(format: "loginUser = %@", login!)
+        
+        do{
+            let _contact = Contacts(context: context)
+            _contact.login = login
+            
+            
+            let user = try context.fetch(fetchRequest).first
+            
+            guard let curUser = user else {
+                return nil
+            }
+            
+            let _contacts = curUser.contacts?.allObjects as? [Contacts]
+            guard let contacts = _contacts else {
+                return nil
+            }
+            
+            var curContacts = [String:Contact]()
+            
+            
+            for contact in contacts {
+                let newContact = Contact(with: contact.login!, name: contact.login!, surname: nil)
+                curContacts[contact.login!] = newContact
+            }
+            
+            
+            return curContacts
+            
+            
+        }catch (let error){
+            print(error)
+            
+            return nil
+        }
+        
+    }
+    
+    func addContact(contact : Contact){
+        self.backQueue.async(flags: .barrier, execute: {  [weak self] in
+            guard let self = self else {return}
+            
+            let context =  self.backgroundContext
+            
+            
+            let fetchRequest = NSFetchRequest<User>(entityName: "User")
+            fetchRequest.predicate =  NSPredicate(format: "loginUser = %@", self.login!)
+            
+            
+            do{
+                let _contact = Contacts(context: context)
+                _contact.login = contact.login
+                
+                let users = try context.fetch(fetchRequest)
+                if let user = users.first(where: {$0.loginUser == self.login})
+                {
+                    
+                    _contact.addToUser(user)
+                    
+                    do{
+                        
+                        try context.save()
+                    }
+                    catch (let error)
+                    {
+                        print(error)
+                        
+                    }
+                    
+                }
+                
+                
+                
+            }catch (let error){
+                print(error)
+                
+                return
+            }
+        })
+    }
+    
+    func addMessages(messages : ChatBody){
+        
+        self.backQueue.async(flags: .barrier, execute: {  [weak self] in
+            guard let self = self else {return}
+            let context = self.backgroundContext
+            
+            let fetchRequest = NSFetchRequest<User>(entityName: "User")
+            fetchRequest.predicate =  NSPredicate(format: "loginUser = %@", self.login!)
+            
+            
+            do{
+                let users = try context.fetch(fetchRequest)
+                if let user = users.first(where: {$0.loginUser == self.login})
+                {
+                    let _chats = user.chats?.allObjects as? [Chats]
+                    guard let chats = _chats else {
+                        return
+                    }
+                    
+                    guard let chat = chats.first(where: {$0.id! == messages.chatId}) else {
+                        return
+                    }
+                    
+                    var chatMessages = try self.decoder.decode(ChatBody.self, from: chat.body!)
+                    chatMessages.messages.append(contentsOf: messages.messages)
+                    
+                    let data = try self.encoder.encode(chatMessages)
+                    
+                    chat.body = data
+                    
+                    
+                    do{
+                        
+                        try context.save()
+                    }
+                    catch (let error)
+                    {
+                        print(error)
+                        
+                    }
+                    
+                    
+                }
+                
+                
+                
+            }catch (let error){
+                print(error)
+                
+                return
+            }
+            
+        })
+    }
     
     // MARK: - Core Data Saving support
     
